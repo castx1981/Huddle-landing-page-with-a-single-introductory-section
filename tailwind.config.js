7a96f614f2298e41a1bca9e64355010b96d509e7
Xiaoliang Luan <helix_ogg@hotmail.com>
2023-04-18 11:03:26 +0800

    desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,23 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    extend: {
+      fontFamily: {
+        'open-sans': ['"Open Sans"', 'sans-serif'],
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'primary-violet': 'hsl(257, 40%, 49%)',
+        'soft-magenta': 'hsl(300, 69%, 71%)'
+      },
+      backgroundImage: {
+        'mobile': "url('images/bg-mobile.svg')",
+        'desktop': "url('images/bg-desktop.svg')",
+      },
+      width: {
+        '3.5': '0.875rem',
+      },
+    }
   },
   plugins: [],
 }
